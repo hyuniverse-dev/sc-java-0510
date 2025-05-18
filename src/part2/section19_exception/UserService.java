9e3eeb26d96a583be003c1cd9d8326a9fb0acb85
hyuniverse-dev <hyuniverse.dev@gmail.com>
2025-05-18 15:01:33 +0900

    custom exception

--- src/part2/section19_exception/UserService.java
+++ src/part2/section19_exception/UserService.java
@@ -15,6 +15,10 @@ public class UserService {
         // 사용자가 입력한 아이디가 저장되어 있는 아이디와 다르면 UserIdException 예외 발생
         // 사용자가 입력한 아이디가 저장되어 있는 비밀번호와 다르면 UserPasswordException 예외 발생
         // 각 예외의 종류별로 예외 처리 -> printstacktrace() 메소드 호출해서 예외 정보 모두 출력하기
-        userA.validIdAndPassword(userId, userPassword);
+        try {
+            userA.validIdAndPassword(userId, userPassword);
+        } catch (UserIdException | UserPasswordException e) {
+            e.printStackTrace();
+        }
     }
 }
